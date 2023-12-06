ffc72c3b67a15f254ab0afef3b23afb1d9d8c976
LysikovIE <lysikovie@gmail.com>
2023-12-06 12:11:46 +0500

    attempt to import my testModule

---
+++ go.mod
@@ -0,0 +1,5 @@
+module useTestModule
+
+go 1.21.5
+
+require github.com/NodeLie/testModule v0.0.0-20231206065104-374a98310027
---
+++ main.go
@@ -0,0 +1,12 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/NodeLie/testModule"
+)
+
+func main() {
+
+	fmt.Println(testModule.Sum(1, 2))
+}
